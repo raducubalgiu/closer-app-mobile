131f27294d03767ac808b725a77b0baac5e89121
Raducu Balgiu <raducu.balgiu@gmail.com>
2022-10-16 10:33:20 +0300

    Remove AppLoading Fonts

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
+import React from "react";
 import { enableScreens } from "react-native-screens";
 import CloserNavigation from "./navigation/CloserNavigation";
 import { AuthProvider } from "./hooks/auth";
@@ -10,33 +8,7 @@ import "./firebase/firebase.config";
 
 enableScreens();
 
-// const fetchFonts = () => {
-//   return Font.loadAsync({
-//     "Exo-Black": require("./assets/fonts/static/Exo-Black.ttf"),
-//     "Exo-Bold": require("./assets/fonts/static/Exo-Bold.ttf"),
-//     "Exo-SemiBold": require("./assets/fonts/static/Exo-SemiBold.ttf"),
-//     "Exo-ExtraBold": require("./assets/fonts/static/Exo-ExtraBold.ttf"),
-//     "Exo-Light": require("./assets/fonts/static/Exo-Light.ttf"),
-//     "Exo-Regular": require("./assets/fonts/static/Exo-Regular.ttf"),
-//     "Exo-ExtraLight": require("./assets/fonts/static/Exo-ExtraLight.ttf"),
-//     "Exo-Medium": require("./assets/fonts/static/Exo-Medium.ttf"),
-//     "Exo-SemiBold": require("./assets/fonts/static/Exo-SemiBold.ttf"),
-//   });
-// };
-
 const App = () => {
-  // const [fontLoaded, setFontLoaded] = useState(false);
-
-  // if (!fontLoaded) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={fetchFonts}
-  //       onFinish={() => setFontLoaded(true)}
-  //       onError={() => setFontLoaded(false)}
-  //     />
-  //   );
-  // }
-
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
