45402a6638d6d03e9180475e7520ce7e420119d2
Raducu Balgiu <raducu.balgiu@gmail.com>
2022-11-02 21:52:06 +0200

    Adding useQuery to hashtags posts

--- components/customized/Tabs/HashtagPostsTabs/HashtagPostsPopularTab.js
+++ components/customized/Tabs/HashtagPostsTabs/HashtagPostsPopularTab.js
@@ -1,7 +1,7 @@
 import { FlatList } from "react-native";
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { useHttpGet } from "../../../../hooks";
+import { useGetPaginate } from "../../../../hooks";
 import { CardPostImage } from "../../Cards/CardPostImage";
 import { NoFoundMessage } from "../../NotFoundContent/NoFoundMessage";
 import { Spinner } from "../../../core";
@@ -9,9 +9,12 @@ import { Spinner } from "../../../core";
 export const HashtagPostsPopularTab = ({ name }) => {
   const { t } = useTranslation();
 
-  const { data: posts, loading: loadPosts } = useHttpGet(
-    `/hashtags/${name}/posts/popular`
-  );
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } =
+    useGetPaginate({
+      model: "hashtagPostsPopular",
+      uri: `/hashtags/${name}/posts/popular`,
+      limit: "20",
+    });
 
   const renderPosts = useCallback(({ item, index }) => {
     const { images, bookable, postType } = item;
@@ -28,23 +31,43 @@ export const HashtagPostsPopularTab = ({ name }) => {
     );
   }, []);
 
-  const noFoundPosts = (
+  const keyExtractor = useCallback((item) => item._id, []);
+
+  const noFoundMessage = (
     <NoFoundMessage title={t("posts")} description={t("noFoundPosts")} />
   );
 
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const showSpinner = () => {
+    if (isFetchingNextPage) {
+      return <Spinner />;
+    } else {
+      return null;
+    }
+  };
+
+  const { pages } = data || {};
+
   return (
-    <>
-      {!posts?.length && !loadPosts && noFoundPosts}
-      {!loadPosts && (
-        <FlatList
-          data={posts}
-          keyExtractor={(item) => item._id}
-          numColumns={3}
-          renderItem={renderPosts}
-          showsVerticalScrollIndicator={false}
-        />
-      )}
-      {loadPosts && <Spinner />}
-    </>
+    <FlatList
+      ListHeaderComponent={
+        !isLoading &&
+        !isFetchingNextPage &&
+        pages[0]?.results?.length === 0 &&
+        noFoundMessage
+      }
+      numColumns={3}
+      data={pages?.map((page) => page.results).flat()}
+      keyExtractor={keyExtractor}
+      renderItem={renderPosts}
+      ListFooterComponent={showSpinner}
+      onEndReached={loadMore}
+      onEndReachedThreshold={0.3}
+    />
   );
 };
