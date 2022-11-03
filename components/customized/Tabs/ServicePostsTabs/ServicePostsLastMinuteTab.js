3fb6c0df3bf264502133b4305d3d868fbff24972
Raducu Balgiu <raducu.balgiu@gmail.com>
2022-11-03 08:04:03 +0200

    Refactoring Service Posts with useQuery

--- components/customized/Tabs/ServicePostsTabs/ServicePostsLastMinuteTab.js
+++ components/customized/Tabs/ServicePostsTabs/ServicePostsLastMinuteTab.js
@@ -1,14 +1,22 @@
 import { FlatList } from "react-native";
 import React, { useCallback } from "react";
-import { useHttpGet } from "../../../../hooks";
+import { useGetPaginate } from "../../../../hooks";
 import { CardPostImage } from "../../Cards/CardPostImage";
+import { NoFoundMessage } from "../../NotFoundContent/NoFoundMessage";
+import { useTranslation } from "react-i18next";
+import { Spinner } from "../../../core";
 
 export const ServicePostsLastMinuteTab = ({ serviceId }) => {
-  const { data: posts } = useHttpGet(
-    `/services/${serviceId}/posts/last-minute`
-  );
+  const { t } = useTranslation();
 
-  const renderPost = useCallback(({ item, index }) => {
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } =
+    useGetPaginate({
+      model: "servicePostsLastMinute",
+      uri: `/services/${serviceId}/posts/last-minute`,
+      limit: "20",
+    });
+
+  const renderPosts = useCallback(({ item, index }) => {
     const { images, bookable, postType } = item;
 
     return (
@@ -23,13 +31,43 @@ export const ServicePostsLastMinuteTab = ({ serviceId }) => {
     );
   }, []);
 
+  const keyExtractor = useCallback((item) => item._id, []);
+
+  const noFoundMessage = (
+    <NoFoundMessage title={t("posts")} description={t("noFoundPosts")} />
+  );
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const showSpinner = () => {
+    if (isFetchingNextPage) {
+      return <Spinner />;
+    } else {
+      return null;
+    }
+  };
+
+  const { pages } = data || {};
+
   return (
     <FlatList
-      data={posts}
-      keyExtractor={(item) => item._id}
-      renderItem={renderPost}
+      ListHeaderComponent={
+        !isLoading &&
+        !isFetchingNextPage &&
+        pages[0]?.results?.length === 0 &&
+        noFoundMessage
+      }
       numColumns={3}
-      showsVerticalScrollIndicator={false}
+      data={pages?.map((page) => page.results).flat()}
+      keyExtractor={keyExtractor}
+      renderItem={renderPosts}
+      ListFooterComponent={showSpinner}
+      onEndReached={loadMore}
+      onEndReachedThreshold={0.3}
     />
   );
 };
--- components/customized/Tabs/ServicePostsTabs/ServicePostsRecentTab.js
+++ components/customized/Tabs/ServicePostsTabs/ServicePostsRecentTab.js
@@ -1,12 +1,22 @@
 import { FlatList } from "react-native";
 import React, { useCallback } from "react";
-import { useHttpGet } from "../../../../hooks";
+import { useGetPaginate } from "../../../../hooks";
 import { CardPostImage } from "../../Cards/CardPostImage";
+import { useTranslation } from "react-i18next";
+import { NoFoundMessage } from "../../NotFoundContent/NoFoundMessage";
+import { Spinner } from "../../../core";
 
 export const ServicePostsRecentTab = ({ serviceId }) => {
-  const { data: posts } = useHttpGet(`/services/${serviceId}/posts/recent`);
+  const { t } = useTranslation();
 
-  const renderPost = useCallback(({ item, index }) => {
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } =
+    useGetPaginate({
+      model: "servicePostsRecent",
+      uri: `/services/${serviceId}/posts/recent`,
+      limit: "20",
+    });
+
+  const renderPosts = useCallback(({ item, index }) => {
     const { images, bookable, postType } = item;
 
     return (
@@ -21,13 +31,43 @@ export const ServicePostsRecentTab = ({ serviceId }) => {
     );
   }, []);
 
+  const keyExtractor = useCallback((item) => item._id, []);
+
+  const noFoundMessage = (
+    <NoFoundMessage title={t("posts")} description={t("noFoundPosts")} />
+  );
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const showSpinner = () => {
+    if (isFetchingNextPage) {
+      return <Spinner />;
+    } else {
+      return null;
+    }
+  };
+
+  const { pages } = data || {};
+
   return (
     <FlatList
-      data={posts}
-      keyExtractor={(item) => item._id}
-      renderItem={renderPost}
+      ListHeaderComponent={
+        !isLoading &&
+        !isFetchingNextPage &&
+        pages[0]?.results?.length === 0 &&
+        noFoundMessage
+      }
       numColumns={3}
-      showsVerticalScrollIndicator={false}
+      data={pages?.map((page) => page.results).flat()}
+      keyExtractor={keyExtractor}
+      renderItem={renderPosts}
+      ListFooterComponent={showSpinner}
+      onEndReached={loadMore}
+      onEndReachedThreshold={0.3}
     />
   );
 };
