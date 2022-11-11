265e0273a9c7e71ec7c22acca8a706bc625717b0
Raducu Balgiu <raducu.balgiu@gmail.com>
2022-11-11 12:11:17 +0200

    Adding conditional count in MessageItemScreen

--- components/customized/Cards/CardMessageUser.js
+++ components/customized/Cards/CardMessageUser.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text } from "react-native";
 import theme from "../../../assets/styles/theme";
 import { Stack, CustomAvatar } from "../../core";
+import { displayCount } from "../../../utils";
+import { useTranslation } from "react-i18next";
 
 const { grey0 } = theme.lightColors;
 
@@ -12,15 +14,31 @@ export const CardMessageUser = ({
   followersCount,
   sx,
 }) => {
+  const { t } = useTranslation();
+
   return (
     <Stack sx={{ marginVertical: 40, ...sx }}>
       <CustomAvatar avatar={avatar} size={100} />
       <Text style={styles.name}>{name}</Text>
       <Text style={{ color: grey0 }}>@{username}</Text>
       <Stack direction="row">
-        <Text style={styles.count}>{followingsCount} de urmariri</Text>
+        <Text style={styles.count}>
+          {displayCount(
+            followingsCount,
+            t("oneFollowing"),
+            t("followings"),
+            t("ofFollowings")
+          )}
+        </Text>
         <Text> - </Text>
-        <Text style={styles.count}>{followersCount} de urmaritori</Text>
+        <Text style={styles.count}>
+          {displayCount(
+            followersCount,
+            t("follower"),
+            t("followers"),
+            t("ofFollowers")
+          )}
+        </Text>
       </Stack>
     </Stack>
   );
@@ -33,5 +51,5 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 2.5,
   },
-  count: { color: grey0, fontSize: 13 },
+  count: { color: grey0, fontSize: 13, textTransform: "lowercase" },
 });
