3043868b398ec5585482478c4e1c3d4f7da6b4ee
Raducu Balgiu <raducu.balgiu@gmail.com>
2022-11-17 15:22:53 +0200

    Making Changes in TopTabNavigator

--- components/customized/Tabs/TopTabContainer/TopTabContainer.js
+++ components/customized/Tabs/TopTabContainer/TopTabContainer.js
@@ -1,7 +1,6 @@
 import { Dimensions, StyleSheet } from "react-native";
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { Icon } from "@rneui/themed";
 import theme from "../../../../assets/styles/theme";
 
 const { width } = Dimensions.get("window");
@@ -10,62 +9,22 @@ const { black } = theme.lightColors;
 export const TopTabContainer = ({
   children,
   initialRouteName,
-  profileTabs,
   tabBarScrollEnabled,
   options,
 }) => {
   const Tab = createMaterialTopTabNavigator();
 
-  let screenOptions;
-  if (profileTabs) {
-    screenOptions = ({ route }) => ({
-      tabBarIcon: ({ focused, color }) => {
-        let iconName;
-        let iconType;
-        let size = 22;
-
-        if (route.name === "Posts") {
-          iconType = "feather";
-          iconName = focused ? "grid" : "grid";
-        } else if (route.name === "Calendar") {
-          iconType = "feather";
-          iconName = focused ? "clock" : "clock";
-        } else if (route.name === "Jobs") {
-          iconType = "feather";
-          iconName = focused ? "briefcase" : "briefcase";
-        } else if (route.name === "About") {
-          iconType = "feather";
-          iconName = focused ? "user-check" : "user-check";
-        }
-        return (
-          <Icon name={iconName} type={iconType} color={color} size={size} />
-        );
-      },
-      tabBarActiveTintColor: black,
-      tabBarInactiveTintColor: "#ccc",
-      headerShown: false,
-      tabBarShowLabel: false,
-      tabBarIndicatorStyle: {
-        backgroundColor: black,
-        marginBottom: 0.5,
-      },
-      lazy: true,
-    });
-  } else {
-    screenOptions = {
-      tabBarActiveTintColor: theme.lightColors.black,
-      tabBarLabelStyle: styles.tabLabel,
-      tabBarStyle: styles.tabStyle,
-      tabBarIndicatorStyle: styles.tabIndicator,
-      tabBarScrollEnabled,
-      ...options,
-    };
-  }
-
   return (
     <Tab.Navigator
       initialRouteName={initialRouteName}
-      screenOptions={screenOptions}
+      screenOptions={{
+        tabBarActiveTintColor: theme.lightColors.black,
+        tabBarLabelStyle: styles.tabLabel,
+        tabBarStyle: styles.tabStyle,
+        tabBarIndicatorStyle: styles.tabIndicator,
+        tabBarScrollEnabled,
+        ...options,
+      }}
       sceneContainerStyle={{ backgroundColor: "white" }}
       initialLayout={{ width, height: 0 }}
     >
--- components/customized/Tabs/TopTabContainer/TopTabProfile.js
+++ components/customized/Tabs/TopTabContainer/TopTabProfile.js
@@ -1,64 +1,97 @@
-import React, { useCallback } from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { TopTabContainer } from "./TopTabContainer";
 import { PostsProfileTab } from "../ProfileTabs/PostsProfileTab";
 import { ProductsProfileTab } from "../ProfileTabs/ProductsProfileTab";
 import { JobsProfileTab } from "../ProfileTabs/JobsProfileTab";
 import { AboutProfileTab } from "../ProfileTabs/AboutProfileTab";
 import { TabBadge } from "../TabBadge/TabBadge";
-import { useHttpGet } from "../../../../hooks";
+import { useGet } from "../../../../hooks";
+import theme from "../../../../assets/styles/theme";
+import { Icon } from "@rneui/themed";
+import { THIRD_ROLE } from "@env";
+
+const { black } = theme.lightColors;
 
 export const TopTabProfile = ({ userId, username, service, option, user }) => {
   const Tab = createMaterialTopTabNavigator();
   const { description, website, location, role } = user || {};
 
-  const { data: services } = useHttpGet(
-    `/locations/${user?.location}/services`
-  );
+  const { data: services } = useGet({
+    model: "services",
+    uri: `/locations/${user?.location}/services`,
+  });
 
-  const PostsProfile = useCallback(
-    () => <PostsProfileTab userId={userId} username={username} />,
-    [userId, username]
+  const PostsProfile = () => (
+    <PostsProfileTab userId={userId} username={username} />
   );
-  const ProductsProfile = useCallback(
-    () => (
-      <ProductsProfileTab
-        userId={userId}
-        services={services}
-        service={service}
-        option={option}
-      />
-    ),
-    [userId, services, service, option]
+  const ProductsProfile = () => (
+    <ProductsProfileTab
+      userId={userId}
+      services={services}
+      service={service}
+      option={option}
+    />
   );
-  const JobsProfile = useCallback(
-    () => <JobsProfileTab userId={userId} username={username} />,
-    [userId, username]
+  const JobsProfile = () => (
+    <JobsProfileTab userId={userId} username={username} />
   );
-  const AboutProfile = useCallback(
-    () => (
-      <AboutProfileTab
-        biography={description}
-        website={website}
-        location={location}
-        role={role}
-      />
-    ),
-    [description, website, location, role]
+  const AboutProfile = () => (
+    <AboutProfileTab
+      biography={description}
+      website={website}
+      location={location}
+      role={role}
+    />
   );
 
   return (
-    <TopTabContainer initialRouteName="Posts" profileTabs={true}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+          let iconType;
+          let size = 22;
+
+          if (route.name === "Posts") {
+            iconType = "feather";
+            iconName = focused ? "grid" : "grid";
+          } else if (route.name === "Calendar") {
+            iconType = "feather";
+            iconName = focused ? "clock" : "clock";
+          } else if (route.name === "Jobs") {
+            iconType = "feather";
+            iconName = focused ? "briefcase" : "briefcase";
+          } else if (route.name === "About") {
+            iconType = "feather";
+            iconName = focused ? "user-check" : "user-check";
+          }
+          return (
+            <Icon name={iconName} type={iconType} color={color} size={size} />
+          );
+        },
+        tabBarActiveTintColor: black,
+        tabBarInactiveTintColor: "#ccc",
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIndicatorStyle: {
+          backgroundColor: black,
+          marginBottom: 0.5,
+        },
+        lazy: true,
+      })}
+      sceneContainerStyle={{ backgroundColor: "white" }}
+    >
       <Tab.Screen name="Posts" component={PostsProfile} />
-      <Tab.Screen
-        name="Products"
-        component={ProductsProfile}
-        options={{
-          tabBarIcon: () => <TabBadge value={0} />,
-        }}
-      />
-      <Tab.Screen name="Jobs" component={JobsProfile} />
+      {role !== THIRD_ROLE && (
+        <Tab.Screen
+          name="Products"
+          component={ProductsProfile}
+          options={{ tabBarIcon: () => <TabBadge value={0} /> }}
+        />
+      )}
+      {role !== THIRD_ROLE && (
+        <Tab.Screen name="Jobs" component={JobsProfile} />
+      )}
       <Tab.Screen name="About" component={AboutProfile} />
-    </TopTabContainer>
+    </Tab.Navigator>
   );
 };
